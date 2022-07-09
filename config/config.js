3de199c2dbb7d7667da65cc96af1f740c87dfc42
aThatch73 <aaron.thatcher11@gmail.com>
2022-07-09 01:38:14 -0700

    Update config with default value placeholders

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const configs = {
-    hostname: process.env.HOSTNAME,
-    apikey: process.env.API_KEY,
-    port: process.env.PORT,
+    hostname: process.env.HOSTNAME || '<default_hostname>',
+    apikey: process.env.API_KEY || '<default_api_key>',
+    port: process.env.PORT || 8800,
 };
 
 module.exports = {
