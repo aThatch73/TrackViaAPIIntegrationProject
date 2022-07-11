6828545bf7b3aae039fbd5782f2571b7c9a8bc02
aThatch73 <aaron.thatcher11@gmail.com>
2022-07-11 13:28:19 -0700

    Add certs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 
 const configs = require('./config/config').configs;
 const foreignExchangeRoutes = require('./api/routes/foreignExchangeRoutes');
@@ -10,6 +13,18 @@ const app = express();
 app.use('/fx', foreignExchangeRoutes);
 app.use('/', routeValidationMiddleware.noRouteHandler);
 app.use(routeValidationMiddleware.noRouteHandler);
+app.use(cors({
+    origin: '*'
+}));
+
+const key = fs.readFileSync(`${__dirname}/certs/key.pem`, 'utf8');
+const cert = fs.readFileSync(`${__dirname}/certs/cert.pem`, 'utf8');
+const options = {
+    key,
+    cert
+};
+
+const server = https.createServer(options, app);
 
 const port = configs.port || 8800;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, () => console.log(`Listening on port ${port}`));
